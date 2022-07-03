f503a72847034da80d35f76d9849682bedb6c883
SLB974 <slb974run@gmail.com>
2022-07-03 05:41:09 +0400

    adding urls.py files to app and basic html templates

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -7,15 +7,14 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", RedirectView.as_view(url="home/", permanent=True)),
     path("article/", include("article.urls")),
     path("tutorial/", include("tutorial.urls")),
-    path("fabrik/", include("fabrik.urls")),
     path("user/", include("user.urls")),
-    path("accounts", include('allauth.urls')),
-    path("home", include("home.urls")),
-    path("comment", include("comment.urls")),
-    path("mail", include("mail.urls")),
+    path("accounts/", include('allauth.urls')),
+    path("home/", include("home.urls")),
+    path("comment/", include("comment.urls")),
+    path("mail/", include("mail.urls")),
+    path("", RedirectView.as_view(url="home/", permanent=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
