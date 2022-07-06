30655e9e1277da9138ecc90708a020530769c6f6
SLB974 <slb974run@gmail.com>
2022-07-06 09:38:16 +0400

    modifying article.models and registering to admin.site all models

--- app/article/models.py
+++ app/article/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=255)
     tuto = models.BooleanField(default=False)
+    oops=models.BooleanField(default=False)
     content = models.TextField()
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
