f8deb2856af036cd09995ea4023bc6be1744b219
SLB974 <slb974run@gmail.com>
2022-07-06 09:47:45 +0400

    adding home/templatetags

---
+++ app/home/templatetags/home_extras.py
@@ -0,0 +1,23 @@
+from article.models import Article, Category
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.simple_tag(name='details')
+def get_details(value):
+    
+    if value=="category":
+        return '('+ str(Category.objects.count()) + ')'
+
+    elif value=="article":
+        return '('+ str(Article.objects.filter(tuto=False).count()) + ')'
+    
+    elif value=="tuto":
+        return '('+str(Article.objects.filter(tuto=True).count())+')'
+    
+    elif value=='oops':
+        return '('+str(Article.objects.filter(oops=True).count())+')'
+    
+    else:
+        return ''
