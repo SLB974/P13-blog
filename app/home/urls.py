c30750905c9c971bc4dbbef8b07bb6a2ffb485e2
SLB974 <slb974run@gmail.com>
2022-07-08 16:09:53 +0400

    Revert "Revert "refactoring skeleton for enabling complements to root folder'""
    This reverts commit edddb315446ecc31127a41cb8055cdd67afde9c3.

--- app/home/urls.py
+++ app/home/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("category/", views.category, name="category"),
+    path("article/", views.article, name="article"),
+    path("tutorial/", views.tuto, name="tuto"),
+    path("oops/", views.oops, name="oops"),
     
 ]
--- app/home/views.py
+++ app/home/views.py
@@ -2,11 +2,29 @@
 
 import logging
 
+from article.models import Article, Category
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import redirect, render
+from django.views import generic
 
 logger = logging.getLogger(__name__)
 
 def index(request):
     return render(request, 'home/index.html')
+
+def category(request):
+    
+    queryset = Category.objects.all()
+    context = {'result': queryset}
+    return render(request,"home/category.html", context)
+
+def article(request):
+    return render(request,"home/article.html")
+
+def tuto(request):
+    return render(request,"home/tuto.html")
+
+def oops(request):
+    return render(request,"home/oops.html")
+
