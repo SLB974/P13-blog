f5d56ab072a3ab7bcd85612f652d1a537058951f
SLB974 <slb974run@gmail.com>
2022-07-22 10:27:12 +0400

    refactoring parser.py for test processing

--- app/factory/parser.py
+++ app/factory/parser.py
@@ -1,4 +1,5 @@
-"""Parsing input file and generating html_list"""
+"""Parsing input file and generating html_dict"""
+
 import itertools
 
 
@@ -7,36 +8,55 @@ class Parser():
     
     def __init__(self, file):
         self.file = file
-        self.html_list = []
+        self.html_dict={"title":None, 
+                        "category":None, 
+                        "intro":None,
+                        "content":[]}
            
     def generator_list(self):
-        """Parsing input file and returning generator
-        indexing content line by line"""
+        """Reading input file and returning generator indexing content line by line"""
         
         with open(self.file, 'r') as f:
             for index, line in enumerate(f):
                 yield index, line
 
-    def parse(self, current_index=1):
-        """recursively parsing generator_list
-        and appending html_list"""
+    def append_htlm_dict_content(self, type, content):
+        '''appending content to html_dict'''
+        self.html_dict['content'].append([type, content])
+        
+    def update_html_dict(self, key, value):
+        '''updating html_dict'''
+        self.html_dict[key] = value
+        
+    def get_html_dict(self):
+        '''getting html_dict'''
+        return self.parse()
+
+    def parse(self, current_index=0):
+        """recursively parsing generator_list and appending html_dict"""
         
         limit = len(list(self.generator_list())) -1
         
         for index, line in itertools.islice(self.generator_list(), current_index, None):
 
-            line = line.replace('\n','<br>').replace('\\n','<br>')
+            line = line.replace('\n','').replace('\\n','<br>')
             
-            content=None
-          
+            type = None
+            content = None
+         
             if line.startswith( 'title:'):
-                type='title'
-                content=line[6:].strip()
+                self.update_html_dict("title", line[6:].strip())
             
             elif line.startswith( 'category:'):
-                type='category'
-                content=line[9:].split(' ')
+                self.update_html_dict("category", self.get_category(line))
                 
+            elif line.startswith( 'intro:'):
+                self.update_html_dict("intro", self.get_intro(index)[0])
+                
+            elif line=='' or line==None:
+                type='br'
+                content='<br>'
+            
             elif line.startswith( '### '):
                 type='h3'
                 content=line[3:].strip()
@@ -56,24 +76,10 @@ class Parser():
                     content=data[0]
                     current_index = data[1]
                 
-            elif line.startswith( 'intro:'):
-                type='intro'
-                data=self.get_intro(index)
-                if data:
-                    content=data[0]
-                    current_index=data[1]
-            
             elif line.startswith( 'link:'):
                 type='link'
-                data=self.get_link(index)
-                if data:
-                    content=data[0]    
-                    current_index=data[1]
+                content=self.get_link(line)
                 
-            elif line=='<br>' or line=='':
-                type='br'
-                content='<br>'
-            
             else:
                 type='paragraph'
                 data=self.get_paragraph(index)
@@ -82,12 +88,33 @@ class Parser():
                     current_index=data[1]
                     
             if type and content:
-                self.html_list.append((type, content))
+                self.append_htlm_dict_content(type, content)
             
             if index == limit:
-                return self.html_list
+                return self.html_dict
             
             return self.parse(current_index+1)
+
+    def get_category(self, line):
+        '''getting category elements'''
+        line=line.replace('<br>','')
+        return line[10:].strip().split(' ')
+    
+    def get_intro(self, current_index, accumulated_data=''):
+        '''getting intro elements'''
+        
+        for index, line in itertools.islice(self.generator_list(), current_index, None):
+            
+            line = line.replace('\n','').strip()
+            line = line.lstrip('intro: ').strip()
+            
+            if line=='':
+                return accumulated_data, index
+            else:
+                accumulated_data += line
+                return self.get_intro(index+1, accumulated_data)
+        
+        return accumulated_data, current_index + 1                
 
     def get_code(self, current_index, accumulated_data=''):
         '''recusively finding code content'''
@@ -100,44 +127,28 @@ class Parser():
                 return accumulated_data, index
             else:
                     accumulated_data += line
-                    return self.get_code(index, accumulated_data)
-        
-    def get_link(self, current_index):
+                    return self.get_code(index + 1, accumulated_data)
+                
+    def get_link(self, line):
         '''getting link elements'''
-        
-        for index, line in itertools.islice(self.generator_list(), current_index + 1, 1):
-                data = line[6:]
-                data = data.lstrip('[').rstrip(']').strip()
-                return data, index
+        return line[6:].lstrip('[').rstrip(']').strip().split(', ')
+
         
     def get_paragraph(self, current_index, accumulated_data=''):
         '''recursively finding paragraph content'''
         
         for index, line in itertools.islice(self.generator_list(), current_index, None):
             
-            line = line.replace('\n','<br>').replace('\\n','<br>')
+            line = line.replace('\n','').strip()
             
-            if line=='<br>' or line=='':
-                return accumulated_data + '<br>', index
+            if line=='':
+                return accumulated_data, index
             else:
                     accumulated_data += line
                     return self.get_paragraph(index+1, accumulated_data)
         
-        return accumulated_data, current_index
+        return accumulated_data, current_index + 1
 
-    def get_intro(self, current_index, accumulated_data=''):
-        '''getting intro elements'''
-        
-        for index, line in itertools.islice(self.generator_list(), current_index, None):
-            
-            line = line.replace('\n','<br>').replace('\\n','<br>').lstrip('intro: ')
-            
-            if line=='<br>':
-                return accumulated_data + '<br>', index
-            else:
-                accumulated_data += line
-                return self.get_intro(index+1, accumulated_data)
-                        
                         
                     
                 
