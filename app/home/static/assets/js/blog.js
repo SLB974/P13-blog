a518279a300842f098a5feea6ca6ce4657170210
SLB974 <slb974run@gmail.com>
2022-07-28 17:15:04 +0400

    refactoring .js files

--- app/home/static/assets/js/blog.js
+++ app/home/static/assets/js/blog.js
@@ -1,11 +1,10 @@
-
 $(document).ready(function() {
     enlight_menu();
+    $(".darkon").click(darkon_click);
+    $(".darkoff").click(darkoff_click);
 })
 
 function enlight_menu() {
-    /* var menu = document.getElementById('home');
-        menu.classList.add('enlighted'); */
         var path=window.location.pathname;
 
         switch(path){
@@ -33,3 +32,14 @@ function enlight_menu() {
 
 }
 
+function darkon_click() {
+    $(".darkon").css("display", "none");
+    $(".darkoff").css("display", "block");
+    document.body.setAttribute("data-theme", "dark");
+};
+
+function darkoff_click() {
+    $(".darkoff").css("display", "none");
+    $(".darkon").css("display", "block");
+    document.body.removeAttribute("data-theme");
+};
