20ae0a352a6831144f23b920c51a22f338e0958e
SLB974 <slb974run@gmail.com>
2022-08-05 08:18:18 +0400

    adding mail_me fonctionnality

--- app/mail/urls.py
+++ app/mail/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns=[
     path("send_mail/", views.mail_me, name="mail_me"),
+    path("mail_sent/", views.mail_sent, name="mail_sent"),
+    path("mail_error/", views.mail_error, name="mail_error"),
     
 ]
--- app/mail/views.py
+++ app/mail/views.py
@@ -7,12 +7,23 @@ from django.shortcuts import render
 @login_required
 def mail_me(request):
     if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        message = request.POST['message']
-        send_mail(
-            'Message from ' + name,
-            message,
-            email)
+        name = request.user.username
+        email = request.user.email
+        subject = request.POST['mail-object']
+        message = request.POST['mail-text']
+        try:
+            send_mail(
+                subject + ' from ' + name + ' (' + email + ')',
+                message,
+                'blog@slb-fullweb.tech',
+                ['contact@slb-fullweb.tech'])
+            return render(request, 'mail/mail_sent.html')
+        except:
+            return render(request, 'mail/mail_error.html')
     return render(request, 'mail/mail_me.html')
-        
+
+def mail_error(request):
+    return render(request, 'mail/mail_error.html')
+
+def mail_sent(request):
+    return render(request, 'mail/mail_sent.html')
