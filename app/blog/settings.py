eaa3537a75a1b5500ffea8600fc627689b330850
SLB974 <slb974run@gmail.com>
2022-08-10 19:38:33 +0400

    fixing error messages

--- app/blog/settings.py
+++ app/blog/settings.py
@@ -21,7 +21,7 @@ else:
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
-DEBUG = False
+DEBUG = bool(os.getenv('DJANGO_DEBUG', default=False))
 hosts = str(os.getenv('DJANGO_ALLOWED_HOSTS'))
 if hosts:
     ALLOWED_HOSTS = hosts.split(',')
